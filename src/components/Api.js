e9f4c5fb132561669e16dcfaa48cdb17b235ea92
lestgabo <lestgabo@gmail.com>
2019-11-18 03:19:38 -0800

    Add information. Create champDropRates and metaCompositions

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
-import ChampDropRate from './ChampDropRates';
-
 class API extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div>Hello did it update?</div>
+                <h1>
+                    <strong>
+                        API data for{' '}
+                        <a href="https://www.tft-helper.com/" target="_blank">
+                            TFT Helper's
+                        </a>{' '}
+                        champ drop rates and meta compositions.
+                    </strong>
+                </h1>
                 <div>
-                    <p>
-                        <ChampDropRate />
-                    </p>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/lestgabo/tft-helper-api" target="_blank">
+                                Edit data here.
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://lestgabo.github.io/tft-helper-api/champDropRates.json" target="_blank">
+                                JSON of champ drop rates.
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://lestgabo.github.io/tft-helper-api/metaCompositions.json" target="_blank">
+                                JSON of meta composition.
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </React.Fragment>
         );
