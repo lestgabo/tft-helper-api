3087995502bb9787fd5b2dfe4809522a03f401a0
lestgabo <lestgabo@gmail.com>
2019-11-18 03:30:34 -0800

    Updates

--- precache-manifest.819da55107af357080dbe545d491cb87.js
+++ precache-manifest.819da55107af357080dbe545d491cb87.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "35423e258253dd1092ce59e8c939d3f4",
+    "revision": "627d267c2c7c6d058fb8a50c2c1a9f84",
     "url": "/tft-helper-api/index.html"
   },
   {
-    "revision": "337d0d3d60eb3324702a",
+    "revision": "13838eb17b5301030b56",
     "url": "/tft-helper-api/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tft-helper-api/static/js/2.2b695c32.chunk.js"
   },
   {
-    "revision": "337d0d3d60eb3324702a",
-    "url": "/tft-helper-api/static/js/main.4f274e91.chunk.js"
+    "revision": "13838eb17b5301030b56",
+    "url": "/tft-helper-api/static/js/main.ebf321bd.chunk.js"
   },
   {
     "revision": "5a1d1f5fa292342d50ae",
